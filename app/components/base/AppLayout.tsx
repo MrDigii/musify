fba9e263187d640aebab26cfe560bd125a102d48
MrDigii <strandlaefer@googlemail.com>
2023-08-05 01:52:12 +0200

    adding login test

--- app/components/base/AppLayout.tsx
+++ app/components/base/AppLayout.tsx
@@ -1,7 +1,11 @@
 import { FC } from 'react';
 
 const AppLayout: FC<{ children?: React.ReactNode }> = ({ children }) => {
-    return <div>{children}</div>;
+    return (
+        <div className="flex justify-center items-center fixed top-0 left-0 w-full h-full overflow-auto bg-gradient-to-r from-cyan-500 to-blue-500">
+            <main className="block p-5">{children}</main>
+        </div>
+    );
 };
 
 export default AppLayout;
